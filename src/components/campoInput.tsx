16221c7d0558d3f60be69bb0500b5c080c122f40
VLira96 <lira94634@gmail.com>
2024-07-03 14:22:41 -0300

    colocando required

--- src/components/campoInput.tsx
+++ src/components/campoInput.tsx
@@ -2,7 +2,7 @@ import ICampoInput from "../interfaces/ICampoInput";
 
 const CampoInput = (props: ICampoInput) => {
     return (
-        <input className="bg-yellow-dark w-full rounded-xl px-4 py-2 text-2xl placeholder:text-brown-taupe placeholder:font-bold shadow-lg" type='text' placeholder={props.placeholder} />
+        <input className="bg-yellow-dark w-full rounded-xl px-4 py-2 text-2xl placeholder:text-brown-taupe placeholder:font-bold shadow-lg" type='text' placeholder={props.placeholder} required />
     );
 }
 
