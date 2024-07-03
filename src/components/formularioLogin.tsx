b0e09c7a67149b3db2d5c5c8c6fe1fafd261bdf0
VLira96 <lira94634@gmail.com>
2024-07-03 18:12:09 -0300

    colocando alert

--- src/components/formularioLogin.tsx
+++ src/components/formularioLogin.tsx
@@ -10,16 +10,15 @@ const FormularioLogin = () => {
     listaUser.push(new User('Felipe', 'felipe@email.com', 'felipe123'));
     listaUser.push(new User('Rilben', 'rilben@email.com', 'rilben123'));
     
-
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     
     const handleSubmit = () => {
-        
+        alert("eeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
     }
 
     return (
-        <form className="flex flex-col items-center gap-12">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center gap-12">
             <CampoInput placeholder="Email" />
             <CampoInput placeholder="Senha" />
             <button className="bg-brown-taupe text-gray-light text-3xl font-bold px-7 pb-2 pt-1 rounded-full shadow-lg" type="submit">ENTRAR</button>
